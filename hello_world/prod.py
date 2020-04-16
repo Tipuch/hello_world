fd3752696e2de88781b34fd03f1fe7d1570564b6
Tipuch <fiorini751@gmail.com>
2020-04-16 18:35:45 -0400

    - fixed environment variables

--- hello_world/prod.py
+++ hello_world/prod.py
@@ -2,10 +2,10 @@ from .settings import *
 
 ALLOWED_HOSTS = (os.environ["ALLOWED_HOST"],)
 
-MEDIA_ROOT = os.environ["MEDIA_PATH"]
+MEDIA_ROOT = os.environ["MEDIA_ROOT"]
 MEDIA_URL = os.environ["MEDIA_URL"]
 
-STATIC_ROOT = os.environ["STATIC_PATH"]
+STATIC_ROOT = os.environ["STATIC_ROOT"]
 STATIC_URL = os.environ["STATIC_URL"]
 
 SECRET_KEY = os.environ["SECRET_KEY"]
